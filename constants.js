d044e34fbd5537ec1148fd600ba79410fc0622b1
Leonardo Mayoral <mayoral.leonardo99@gmail.com>
2022-10-13 14:41:59 -0300

    changed colors

--- constants.js
+++ constants.js
@@ -1,8 +1,8 @@
 export const constants = {
   styles: {
-    primaryColor: '#610000',
-    secondaryColor: '#FFE9A1',
-    tertiaryColor: '#BD2A2A',
+    primaryColor: '#360568',
+    secondaryColor: '#7785AC',
+    tertiaryColor: '#A5E6BA',
     paddingVertical: 120
   }
 }
